4222d3503b9fd74c27eff47e8908d317150419b7
Sally Young <sally@justafish.co.uk>
2018-06-08 12:29:44 +0100

    lolphp

--- core/modules/rest/tests/src/Functional/EntityResource/XmlEntityNormalizationQuirksTrait.php
+++ core/modules/rest/tests/src/Functional/EntityResource/XmlEntityNormalizationQuirksTrait.php
@@ -95,10 +95,10 @@ trait XmlEntityNormalizationQuirksTrait {
             $width = (string) $width;
             $target_id = &$normalization[$field_name][$i]['target_id'];
             $target_id = (string) $target_id;
-            if ($normalization[$field_name][$i]['derivatives']) {
+            if ($normalization[$field_name][$i]['derivatives'] && is_array($normalization[$field_name][$i]['derivatives'])) {
               $derivatives = &$normalization[$field_name][$i]['derivatives'];
               if (is_array($derivatives)) {
-                foreach ($derivatives as $image_style => $value) {
+                foreach (array_keys($derivatives) as $image_style) {
                   $derivatives[$image_style]['width'] = (string) $derivatives[$image_style]['width'];
                   $derivatives[$image_style]['height'] = (string) $derivatives[$image_style]['height'];
                 }
