535733d3328585c73175cc0a085d0d4abc3ba2c4
Sally Young <sally@justafish.co.uk>
2018-06-08 12:09:50 +0100

    Fix int to string conversion for XML test

--- core/modules/rest/tests/src/Functional/EntityResource/XmlEntityNormalizationQuirksTrait.php
+++ core/modules/rest/tests/src/Functional/EntityResource/XmlEntityNormalizationQuirksTrait.php
@@ -95,6 +95,15 @@ trait XmlEntityNormalizationQuirksTrait {
             $width = (string) $width;
             $target_id = &$normalization[$field_name][$i]['target_id'];
             $target_id = (string) $target_id;
+            if ($normalization[$field_name][$i]['derivatives']) {
+              $derivatives = &$normalization[$field_name][$i]['derivatives'];
+              if (is_array($derivatives)) {
+                foreach ($derivatives as $image_style => $value) {
+                  $derivatives[$image_style]['width'] = (string) $derivatives[$image_style]['width'];
+                  $derivatives[$image_style]['height'] = (string) $derivatives[$image_style]['height'];
+                }
+              }
+            }
             break;
         }
       }
